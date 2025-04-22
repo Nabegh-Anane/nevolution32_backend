6851f8c80747e399e17d59e344d8202d8f48feac
Nabegh ANANE <contact@nabeghanane.com>
2025-04-22 13:05:34 +0100

    add auth

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,14 @@
+const CLIENT_KEY = 'awfn48k0vef1z2gc';
+const REDIRECT_URI = 'https://nevolution32-backend.vercel.app/api/callback';
+const SCOPES = [
+  'user.info.basic',
+  'video.upload',
+  'user.info.profile',
+  'user.info.stats'
+].join(',');
+
 export default function handler(req, res) {
-    const authUrl = `https://www.tiktok.com/v2/auth/authorize/?client_key=${process.env.CLIENT_KEY}&response_type=code&scope=user.info.basic,user.info.profile,video.upload&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&state=xyz&nonce=xyz&prompt=consent`;
-    res.redirect(authUrl);
-  }
-  
+  const authUrl = `https://www.tiktok.com/v2/auth/authorize/?client_key=${CLIENT_KEY}&scope=${SCOPES}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&state=your_custom_state`;
+
+  res.redirect(authUrl);
+}
